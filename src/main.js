f1c933567e641d4b953ce882c47def36b0878e71
gurenyi <gurenyi92@126.com>
2020-11-29 20:46:30 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSimplemde from 'vue-simplemde'
+import 'simplemde/dist/simplemde.min.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(VueSimplemde) 
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyEditor />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MyEditor from '@/components/MyEditor.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    MyEditor
   }
 }
 </script>
